0e07b00f64fa4d6deabd61b99e10859015020159
Johnnathan Baxter <jbaxter6@radford.edu>
2020-05-10 05:27:12 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,13 +1,19 @@
 def using_include(array, element)
-
+if array.include?(element)
+return TRUE
+else
+  return FALSE
+end
 end
 
 def using_sort(array)
-
+array == array.sort!
+return array
 end
 
 def using_reverse(array)
-
+array.reverse!
+return array
 end
 
 def using_first(array)
